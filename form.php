9f680c4daa4460ac2e308616630c0568dbb3253c
Ashwin143 <ashwinparthasarathy30@gmail.com>
2020-08-10 14:48:45 +0530

    today

---
+++ form.php
@@ -0,0 +1,11 @@
+<html>
+<head><title>Post method</title></head>
+<body>
+ 
+  	<form action=""method="post">
+  		 Name<input type="text"name="user"><br/>
+                 Company<input type="text"name="comp"><br/>
+  	         <input type="submit" value="submit info">
+ 	</form>
+</body>
+</html>
